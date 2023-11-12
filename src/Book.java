53bc3d0e18dc2a9bed5dd5c3738e6bea662fd3b6
datigrov <denistigrov@icloud.com>
2023-11-12 18:13:07 +0300

    ДЗ по теме обьекты и классы №1

---
+++ src/Book.java
@@ -0,0 +1,31 @@
+import javax.xml.namespace.QName;
+
+public class Book {
+    private String name;
+    private Author author;
+    private int publicationYear;
+
+    public Book(){
+    }
+
+    public Book(String name, Author author,int publicationYear) {
+        this.author = author;
+        this.name = name;
+        this.publicationYear = publicationYear;
+    }
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+    public String getName() {
+        return name;
+    }
+    public Author getAuthor() {
+        return author;
+    }
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+}
+
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import javax.swing.text.Position;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Author danSimmons = new Author("Дэн", "Симмонс");
+        Book book = new Book("Террор", danSimmons,2007);
+
+        Author nikolayOstrovskiy = new Author("Николай", "Островский");
+        Book book1 = new Book("Как закалялась сталь", nikolayOstrovskiy, 1934);
+
+
     }
 }
